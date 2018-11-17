da7a3bc6827abf584eaf1a4b89e1ff60b70fd6a5
André P. Bertoletti <apbertoletti@gmail.com>
2018-11-16 23:33:02 -0200

    Adding initial poll service class

---
+++ PollApp.Domain/Interfaces/IPollService.cs
@@ -0,0 +1,16 @@
+﻿using PollApp.Domain.Entities;
+using System.Collections.Generic;
+
+namespace PollApp.Domain.Interfaces
+{
+    public interface IPollService
+    {
+        IEnumerable<Poll> Get();
+
+        Poll GetById(int id);
+
+        Poll Add(Poll poll);
+
+        void Remove(int id);
+    }
+}
