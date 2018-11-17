e496fd769857fecb51b6f072f9174e4ffd058c1b
André P. Bertoletti <apbertoletti@gmail.com>
2018-11-17 00:48:41 -0200

    Adding first unit test

---
+++ PollApp.Test/API/PollControllerTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PollApp.API.Controllers;
+using Xunit;
+
+namespace PollApp.Test.API
+{
+    public class PollControllerTest
+    {
+        #region Properties
+
+        PollController pollController;
+
+        #endregion
+
+        #region Constructors
+
+        public PollControllerTest()
+        {
+            pollController = new PollController();
+        }
+
+        #endregion
+
+        #region Methods
+
+        [Fact]
+        public void GetTest()
+        {
+            var ret = pollController.Get();
+
+            Assert.IsType<OkObjectResult>(ret);
+        }
+
+        #endregion
+    }
+}
