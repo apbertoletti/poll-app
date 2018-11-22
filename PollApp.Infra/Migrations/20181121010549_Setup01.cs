c50864c0901ad37be69730ffb1f891f113987984
André P. Bertoletti <apbertoletti@gmail.com>
2018-11-22 13:13:48 -0200

    Revert "Removing migration files"
    This reverts commit 6fe36709f5c9855c12f0cb211ea00e0aa13da846.

---
+++ PollApp.Infra/Migrations/20181121010549_Setup01.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PollApp.Infra.Migrations
+{
+    public partial class Setup01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Poll",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Poll", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PollOption",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Description = table.Column<string>(maxLength: 200, nullable: false),
+                    Votes = table.Column<int>(nullable: false, defaultValue: 0),
+                    PollID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PollOption", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PollOption_Poll_PollID",
+                        column: x => x.PollID,
+                        principalTable: "Poll",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PollOption_PollID",
+                table: "PollOption",
+                column: "PollID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PollOption");
+
+            migrationBuilder.DropTable(
+                name: "Poll");
+        }
+    }
+}
